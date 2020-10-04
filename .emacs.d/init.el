95df73a2adc00ec60a9e20d5cd3db167692dc7c7
dimax <dimax.devel@gmail.com>
2020-10-04 09:53:27 +0900

    update emacs.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,2 +1,19 @@
 (set-language-environment "Japanese")
 (prefer-coding-system 'utf-8)
+;; "yes or no" の選択を "y or n" にする
+(fset 'yes-or-no-p 'y-or-n-p)
+;; 対応する括弧をハイライト
+(show-paren-mode 1)
+;; current directory 表示
+(let ((ls (member 'mode-line-buffer-identification
+                  mode-line-format)))
+  (setcdr ls
+    (cons '(:eval (concat " ("
+            (abbreviate-file-name default-directory)
+            ")"))
+;; tabサイズ
+(setq default-tab-width 4)
+;; タイトルにフルパス表示
+(setq frame-title-format "%f")
+;; 大文字・小文字を区別しない
+(setq case-fold-search t)

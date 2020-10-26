189fe911394642c0b3c04aa4edceadf7f5ca74e7
dimax <dimax.devel@gmail.com>
2020-10-26 14:56:48 +0900

    add auto-complete package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,7 +11,9 @@
 ;; 言語設定
 (set-language-environment "Japanese")
 (prefer-coding-system 'utf-8)
-
+;; バックアップ／オートセーブを無効化
+(setq make-backup-files nil)
+(setq auto-save-default nil)
 ;; "yes or no" の選択を "y or n" にする
 (fset 'yes-or-no-p 'y-or-n-p)
 ;; 対応する括弧をハイライト
@@ -26,6 +28,49 @@
 (load-theme 'zenburn t)
 ;; C-hで一文字削除
 (global-set-key "\C-h" 'delete-backward-char)
+;; 列/行番号の表示設定
+(column-number-mode 0)
+(line-number-mode 0)
+(global-linum-mode t)
+;; モードラインの文字コード表記をわかりやすくする
+(require 'cl-lib)
+;; 改行文字の文字列表現
+(set 'eol-mnemonic-dos "(CRLF)")
+(set 'eol-mnemonic-unix "(LF)")
+(set 'eol-mnemonic-mac "(CR)")
+(set 'eol-mnemonic-undecided "(?)")
+
+;; 文字エンコーディングの文字列表現
+(defun my-coding-system-name-mnemonic (coding-system)
+  (let* ((base (coding-system-base coding-system))
+         (name (symbol-name base)))
+    (cond ((string-prefix-p "utf-8" name) "U8")
+          ((string-prefix-p "utf-16" name) "U16")
+          ((string-prefix-p "utf-7" name) "U7")
+          ((string-prefix-p "japanese-shift-jis" name) "SJIS")
+          ((string-match "cp\\([0-9]+\\)" name) (match-string 1 name))
+          ((string-match "japanese-iso-8bit" name) "EUC")
+          (t "???")
+          )))
+
+(defun my-coding-system-bom-mnemonic (coding-system)
+  (let ((name (symbol-name coding-system)))
+    (cond ((string-match "be-with-signature" name) "[BE]")
+          ((string-match "le-with-signature" name) "[LE]")
+          ((string-match "-with-signature" name) "[BOM]")
+          (t ""))))
+
+(defun my-buffer-coding-system-mnemonic ()
+  "Return a mnemonic for `buffer-file-coding-system'."
+  (let* ((code buffer-file-coding-system)
+         (name (my-coding-system-name-mnemonic code))
+         (bom (my-coding-system-bom-mnemonic code)))
+    (format "%s%s" name bom)))
+
+;; `mode-line-mule-info' の文字エンコーディングの文字列表現を差し替える
+(setq-default mode-line-mule-info
+              (cl-substitute '(:eval (my-buffer-coding-system-mnemonic))
+                             "%z" mode-line-mule-info :test 'equal))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -34,7 +79,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (jekyll-modes python-mode dockerfile-mode yaml-mode magit zenburn-theme))))
+    (auto-complete jekyll-modes python-mode dockerfile-mode yaml-mode magit zenburn-theme))))
 
 ;; magit設定
 (defalias 'magit 'magit-status)
